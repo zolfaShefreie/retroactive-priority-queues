5c17b1e7c0f49e57e86697fc57fdf8d0215ba47f
zolfaShefreie <z.shefreie@gmail.com>
2022-03-16 17:19:27 +0330

    add some functions for insert a new node

--- retroactive.py
+++ retroactive.py
@@ -57,3 +57,109 @@ class Node(BaseNode):
         self.range_time = (start, end)
 
 
+class RetroactivePriorityQueue:
+    NODE_TYPE = Node
+
+    def __int__(self):
+        self._root_key = None
+        self._items = dict()
+        self._leaves = list()
+        self._new_id = 0
+
+    def _push_non_root(self, new_node: NODE_TYPE, subtree_root: NODE_TYPE):
+        """
+        insert a non-root node to tree
+        :param new_node:
+        :param subtree_root:
+        :return:
+        """
+        # TODO: balance the tree
+        if (subtree_root.left_key is not None) and (subtree_root.right_key is not None):
+            self._update_parent_insert(parent=subtree_root, new_node=new_node)
+            if new_node < subtree_root:
+                self._push_non_root(new_node=new_node, subtree_root=self._items[subtree_root.left_key])
+            else:
+                self._push_non_root(new_node=new_node, subtree_root=self._items[subtree_root.right_key])
+
+        else:
+            if new_node < subtree_root:
+                self._create_subtree(node_left=new_node, node_right=subtree_root, new='left')
+            else:
+                self._create_subtree(node_left=subtree_root, node_right=new_node, new='right')
+
+    def _create_subtree(self, node_left, node_right, new: str):
+        range_time = (min(node_left.range_time[0], node_right.range_time[0]),
+                      max(node_right.range_time[1], node_left.range_time[1]))
+        parent = self.NODE_TYPE(data=node_left.data.merge(node_right.data), range_time=range_time)
+
+        node_left.range_time = (node_left.range_time[0], node_right.range_time[0] - 1)
+        node_right.range_time = (node_right.range_time[0], max(node_right.range_time[1], node_left.range_time[1]))
+
+        parent.right_key = node_right.range_time
+        parent.left_key = node_left.range_time
+
+        if new == 'right':
+            parent.parent_key = node_left.parent_key
+            self._items[parent.parent_key].left_key = parent.range_time
+            self._items.pop(node_left.range_time, None)
+        else:
+            parent.parent_key = node_right.parent_key
+            self._items[parent.parent_key].right_key = parent.range_time
+            self._items.pop(node_right.range_time, None)
+
+        node_right.parent_key = range_time
+        node_left.parent_key = range_time
+
+        self._items.update({range_time: parent, node_right.range_time: node_right, node_left.range_time: node_left})
+
+    def _update_parent_insert(self, parent: Node, new_node: Node):
+        """
+        update range_time and data of parent when a push happened
+        :param parent:
+        :param new_node:
+        :return:
+        """
+        range_time = parent.range_time
+        if range_time[0] < new_node.range_time[0]:
+            range_time = (new_node.range_time[0], range_time[1])
+        elif range_time[1] > new_node.range_time[1]:
+            range_time = (range_time[0], new_node.range_time[1])
+
+        parent.range_time = range_time
+        parent.data.merge(new_node.data)
+        self._items[range_time] = parent
+
+    def _push(self, new_node):
+        """
+        insert a node to tree
+        :param new_node:
+        :return:
+        """
+        if self._root_key is None:
+            self._root_key = new_node.range_time
+            self._items.update({new_node.range_time: new_node})
+
+        else:
+            self._push_non_root(new_node=new_node, subtree_root=self._items[self._root_key])
+
+    def _create_leaf_node(self, time: int, value) -> NODE_TYPE:
+        """
+        create new leaf node
+        :param time:
+        :param value:
+        :return: new node
+        """
+        data = PriorityQueue(started_at=self._new_id).push(value)
+        self._new_id += 1
+        return self.NODE_TYPE(data=data, range_time=(time, time), start_operation=Operations.Push)
+
+    def insert(self, operation, time: int, value=None):
+        if operation == Operations.Push:
+            # TODO check it overlaps in leaf nodes if overlap update
+            new_node = self._create_leaf_node(time, value)
+            self._push(new_node=new_node)
+        elif operation == Operations.Pop:
+            pass
+
+    def delete(self):
+        pass
